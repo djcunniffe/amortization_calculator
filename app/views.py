dc5558c3a5f1188b8dda8fa2cb1ddb5a97bdf5da
djcunniffe <32771602+djcunniffe@users.noreply.github.com>
2018-11-18 09:19:54 +0200

    Schedule generation implemented and styled

--- app/views.py
+++ app/views.py
@@ -5,6 +5,8 @@ from flask import render_template, request, flash
 from app import app
 from app import amor
 import numpy as np
+import pandas as pd
+import datetime as dt
 
 @app.route('/')
 def index():
@@ -14,18 +16,47 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/', methods=['GET','POST'])
-def calculate():
+#@app.route('/', methods=['GET','POST'])
+def calculate(amount,interest,months,currency):
     """
-    Take inputs and caluclate the new payment
+    Take inputs, caluclate and display the new monthly payment
     """
-    amount = float(request.form['amount'])
-    interest = float(request.form['interest'])
-    date = request.form['cycle']
-    months = int(request.form['months'])
-    currency = request.form['currency']
+    #amount = float(request.form['amount'])
+    #interest = float(request.form['interest'])/100
+    #months = int(request.form['months'])
+    #currency = request.form['currency']
 
     pmt = -round(np.pmt(interest/12, months, amount),2)
     flash('The New Payment is: %s %s' % (currency, '{:0,.2f}'.format(pmt)))
     return render_template('index.html')
-    # 'Testing : Amount %s Interest %s Date %s Months %s New Payment %s' % (amount, interest, date, months, pmt)
+
+#@app.route('/schedule', methods=['GET','POST'])
+def schedule(amount,interest,date,months,currency):
+    """
+    Take the inputs and output the amortization schedule
+    """
+    #amount = float(request.form['amount'])
+    #interest = float(request.form['interest'])/100
+    #date = request.form['cycle']
+    #months = int(request.form['months'])
+    #currency = request.form['currency']
+
+    schedule_df = pd.DataFrame(amor.amortize(amount,interest,months,"compound",currency,start_date=dt.datetime.strptime(date,"%Y-%m-%d").date()))
+
+    return render_template('schedule.html', data=schedule_df.to_html(index=False,classes='table table-hover table-responsive'))
+
+@app.route('/', methods=['GET','POST'])
+def display():
+    
+    amount = float(request.form['amount'])
+    interest = float(request.form['interest'])/100
+    date = request.form['cycle']
+    months = int(request.form['months'])
+    currency = request.form['currency']
+    if request.method == 'POST':
+        if request.form['submit'] == 'Calculate':
+            return calculate(amount,interest,months,currency)
+        elif request.form['submit'] == 'Generate Schedule':
+            return schedule(amount,interest,date,months,currency)
+    elif request.method == 'GET':
+        return render_template('index.html')
